29def177ed7ef4aecbb82189f2c426cef60a4735
Solar <93954648+SolarysJS@users.noreply.github.com>
2022-03-23 23:48:16 -0400

    Update main.js

--- scrips/main.js
+++ scrips/main.js
@@ -1,23 +1,16 @@
-Events.on(ClientLoadEvent, () => {
-    const solara = new Planet("solara", Planets.sun, 1, 1);
-    solara.generator = new SerpuloPlanetGenerator();
-    solara.mesh = new HexMesh(routros, 5);
-    solara.orbitRadius = 20;
-    solara.orbitTime = 1.5 * 60;
-    solara.rotateTime = 20;
-    solara.bloom = true;
-    solara.accessible = true;
-    solara.startSector = 1;
-    solara.hasAtmosphere = true;
-    solara.atmosphereColor = Liquids.cryofluid.color;
-    solara.atmosphereRadIn = 0.2;
-    solara.atmosphereRadOut = 0.3;
-    solara.alwaysUnlocked = true;
-    solara.localizedName = "Solara";
+function req(){
+  for(let i of arguments){
+    const dir = (i[0] != "")? i[0] + "/" : "";
+    i.forEach(e => {
+      if(e != i[0]){
+        print("Solarys/" + dir + e)
+        require("Solarys/" + dir + e)
+      }
+    })
+  }
+}
 
-    for(var i = 0; i < 31; i++){
-        var h = new SectorPreset("solara" + i, solara, i);
-        h.localizedName = "Solara" + i;
-        h.alwaysUnlocked = true;
-    };
-});
+req(
+  //Campaign
+  ["campaign", "planets"]
+)
