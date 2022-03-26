f89785aa720b0c5ad55c545ae3592a3d1cab7dd2
Arkanic <93954648+ArkanicXD@users.noreply.github.com>
2022-03-26 00:18:58 -0400

    Update SLBetelgeuse.js

--- scrips/blocks/crafter/SLBetelgeuse.js
+++ scrips/blocks/crafter/SLBetelgeuse.js
@@ -5,20 +5,20 @@ const Betelgeuse = extend(GenericCrafter, "Betelgeuse", {
     this.region = Core.atlas.find(this.name)
     this.topRegion = Core.atlas.find(this.name + "-top")
     this.pointRegion = Core.atlas.find(this.name + "-point")
-    this.phaseRegion = Core.atlas.find(this.name + "-glow")
+    this.phaseRegion = Core.atlas.find(this.name + "-phase")
   }
 })
 Betelgeuse.buildType = () => extend(GenericCrafter.GenericCrafterBuild, Betelgeuse, {
   drawTime: 0,
   drawTime2: 0,
-  drawGlowAlpha: 0,
+  drawPhaseAlpha: 0,
   update() {
     this.super$update()
     var a
     if (this.cons.valid()) { a = 1 } else(a = 0)
     this.drawTime = Mathf.lerp(this.drawTime, this.edelta() * a, 0.05)
     this.drawTime2 += this.drawTime
-    this.drawGlowAlpha = Mathf.lerpDelta(this.drawGlowAlpha, this.progress, 0.2)
+    this.drawPhaseAlpha = Mathf.lerpDelta(this.drawPhaseAlpha, this.progress, 0.2)
     }
   },
 draw() {
